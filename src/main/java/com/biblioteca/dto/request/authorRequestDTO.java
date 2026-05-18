788f001f3a952d529da562eb646bd12e48dbddaa
alexandre <alexandresargitario@gmail.com>
2026-05-18 17:13:15 -0300

    dto author request e response

---
+++ src/main/java/com/biblioteca/dto/request/authorRequestDTO.java
@@ -0,0 +1,12 @@
+package com.biblioteca.dto.request;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class authorRequestDTO {
+    private String name;
+    private String details;
+}
---
+++ src/main/java/com/biblioteca/dto/response/authorResponseDTO.java
@@ -0,0 +1,13 @@
+package com.biblioteca.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class authorResponseDTO {
+    private String name;
+    private String details;
+}
