ae9951a038638e561b1bdf9f83a203aad8a6f742
alexandre <alexandresargitario@gmail.com>
2026-06-19 15:44:28 -0300

    insert de test de book

---
+++ src/main/resources/db/migration/V10__insert_book_test.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_books(id,title,details,pages,created_at,updated_at,status,removed,author_id,category_id)
+VALUES
+(1,'string','string',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'AVAILABLE',false,'11111111-1111-1111-1111-111111111111','aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa');
