ec1a0faeda19f4f1635d98d6769e73356a4fc579
alexandre <alexandresargitario@gmail.com>
2026-06-29 17:44:27 -0300

    mensagem de erro da rota de usuário não está autenticado

--- src/main/java/com/biblioteca/config/SecurityConfig.java
+++ src/main/java/com/biblioteca/config/SecurityConfig.java
@@ -2,14 +2,17 @@ package com.biblioteca.config;
 
 import com.biblioteca.security.JwtFilter;
 import com.biblioteca.security.JwtUtils;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -34,9 +37,25 @@ public class SecurityConfig {
                                         .anyRequest().authenticated()
 
                                 )
+                .exceptionHandling(exception ->
+                        exception.authenticationEntryPoint(authenticationEntryPoint()))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
+    }
+    @Bean
+    AuthenticationEntryPoint authenticationEntryPoint(){
+        return (request, response, authException) -> {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+
+            response.getWriter().write("""
+                    {
+                    "status":401,
+                    "message":"Você precisa estar autenticado."
+                    }
+                    """);
+        };
     }
 
     @Bean
