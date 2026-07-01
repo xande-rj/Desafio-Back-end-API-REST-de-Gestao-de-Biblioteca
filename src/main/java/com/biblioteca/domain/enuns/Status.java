e622363d989223fb1c2e36d9dfa88373c124062d
Alexandre Silva <alexandresargitario@gmail.com>
2026-07-01 18:22:13 -0300

    adicao de descricao nos campos enum Status

--- src/main/java/com/biblioteca/domain/enuns/Status.java
+++ src/main/java/com/biblioteca/domain/enuns/Status.java
@@ -4,6 +4,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Status de disponibilidade")
 public enum Status {
-    AVAILABLE,
-    UNAVAILABLE
+    AVAILABLE("Available"),
+    UNAVAILABLE("Unavailable");
+
+    private final String description;
+
+    Status(String description){
+        this.description = description;
+    }
+    public String getDescription(){
+        return this.description;
+    }
 }
