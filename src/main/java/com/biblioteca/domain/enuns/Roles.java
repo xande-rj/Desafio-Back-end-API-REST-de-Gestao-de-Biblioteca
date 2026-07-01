d6393fd7fa424fe31017db5f7dd3c76921346fee
Alexandre Silva <alexandresargitario@gmail.com>
2026-07-01 18:20:28 -0300

    adicao de descricao nos campos enum Roles

--- src/main/java/com/biblioteca/domain/enuns/Roles.java
+++ src/main/java/com/biblioteca/domain/enuns/Roles.java
@@ -4,7 +4,17 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Roles de usuario")
 public enum Roles {
-    ADMIN,
-    ATTENDANT,
-    READER
+    ADMIN("Admin"),
+    ATTENDANT("Attendant"),
+    READER("Reader");
+
+    private final String description;
+
+    Roles(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
 }
