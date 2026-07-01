5e06dd3fc229e9479df708b571a8ac733f70fe14
Alexandre Silva <alexandresargitario@gmail.com>
2026-07-01 18:05:54 -0300

    adicao de descricao no enum Roles

--- src/main/java/com/biblioteca/domain/enuns/Roles.java
+++ src/main/java/com/biblioteca/domain/enuns/Roles.java
@@ -1,5 +1,8 @@
 package com.biblioteca.domain.enuns;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Roles de usuario")
 public enum Roles {
     ADMIN,
     ATTENDANT,
