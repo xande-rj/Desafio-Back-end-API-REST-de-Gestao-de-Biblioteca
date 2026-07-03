3d49bbe137cdbda0bcfac69aaf0511bb486fff00
Alexandre Silva <alexandresargitario@gmail.com>
2026-07-03 16:08:06 -0300

    documentacao de endpoint controller Loan

--- src/main/java/com/biblioteca/controller/LoanController.java
+++ src/main/java/com/biblioteca/controller/LoanController.java
@@ -5,6 +5,7 @@ import com.biblioteca.dto.request.LoanResquestDTO;
 import com.biblioteca.dto.response.LoanReturnResponseDTO;
 import com.biblioteca.dto.response.LoansResponseDTO;
 import com.biblioteca.dto.response.PenaltyResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,15 +26,27 @@ public class LoanController {
     }
 
     @PostMapping()
+    @Operation(
+            summary = "Create a loan",
+            description = "Creates a new loan at the bank."
+    )
     public ResponseEntity<LoansResponseDTO> addLoan(@RequestBody LoanResquestDTO dto){
         return ResponseEntity.status(200).body(this.loanService.addLoan(dto));
     }
 
     @PatchMapping("/{id}/return")
+    @Operation(
+            summary = "Returns a loan",
+            description = "makes the book available for other loans."
+    )
     public ResponseEntity<LoanReturnResponseDTO> loanReturn(@PathVariable Long id){
         return ResponseEntity.status(200).body(this.loanService.loanReturn(id));
     }
     @GetMapping("/overdue")
+    @Operation(
+            summary = "Returns all overdue loans",
+            description = "returns all overdue loans."
+    )
     public ResponseEntity<List<LoansResponseDTO>> overdueLoans(){
         return new ResponseEntity<>(this.loanService.overdueLoan(), HttpStatus.OK);
     }
