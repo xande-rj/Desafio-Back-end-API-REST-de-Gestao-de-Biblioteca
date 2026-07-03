b82ac5a8952fb223760f980a7e5962d731e2dc22
Alexandre Silva <alexandresargitario@gmail.com>
2026-07-03 16:29:00 -0300

    documentacao de endpoint controller User

--- src/main/java/com/biblioteca/controller/UserController.java
+++ src/main/java/com/biblioteca/controller/UserController.java
@@ -7,6 +7,7 @@ import com.biblioteca.dto.request.UserUpdateDTO;
 import com.biblioteca.dto.response.LoansResponseDTO;
 import com.biblioteca.dto.response.UserResponseDTO;
 import com.biblioteca.dto.response.UserTokenResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -28,24 +29,46 @@ public class UserController {
     }
 
     @PostMapping("/auth/login")
+    @Operation(
+            summary = "User authentication",
+            description = "Verifies a user's credentials."
+    )
     public ResponseEntity<UserTokenResponseDTO> authUser(@RequestBody UserAuthRequestDTO data){
         return new ResponseEntity<>(this.service.authUser(data), HttpStatus.OK);
     }
+
     @PostMapping("/users")
+    @Operation(
+            summary = "Creates a new user",
+            description = "Creates a new user in the database."
+    )
     public ResponseEntity<UserTokenResponseDTO> saveUser(@RequestBody UserRequestDTO data){
         return new ResponseEntity<>(this.service.saveUser(data), HttpStatus.CREATED);
     }
 
     @GetMapping("/users/{id}")
+    @Operation(
+            summary = "Returns a user",
+            description = "Returns a user by ID."
+    )
     public ResponseEntity<UserResponseDTO> getById(@PathVariable Long id){
         return new ResponseEntity<>(this.service.getUseById(id),HttpStatus.OK);
     }
 
     @PutMapping("/users/{id}")
+    @Operation(
+            summary = "Updates a user",
+            description = "Updates a user's data."
+    )
     public ResponseEntity<UserResponseDTO> updateUser(@PathVariable Long id, @RequestBody @Valid UserUpdateDTO userUpdateDTO){
         return new ResponseEntity<>(this.service.updateUser(id,userUpdateDTO),HttpStatus.OK);
     }
+
     @GetMapping("/users/{id}/loans")
+    @Operation(
+            summary = "Returns the loans",
+            description = "returns all loans for a user."
+    )
     public ResponseEntity<List<LoansResponseDTO>> getLoan(@PathVariable Long id){
         return ResponseEntity.status(200).body(this.service.historicalLoans(id));
 
